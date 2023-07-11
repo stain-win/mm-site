ba35e42776d5087154bf43ca80c881e8f41f8360
miroslavmitrovic <stainnn@gmail.com>
2023-07-11 16:15:55 +0200

    feat(mm-lib): bootstrap module add
    Added bootstrap module for app level configuration and basic http error interceptors

--- projects/mm-app/src/app/app.module.ts
+++ projects/mm-app/src/app/app.module.ts
@@ -2,7 +2,9 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
-import {TuiModeModule, TuiRootModule} from '@taiga-ui/core';
+import {BootstrapModule} from '@mm-lib';
+import {TuiAlertModule, TuiModeModule, TuiRootModule, TuiThemeNightModule} from '@taiga-ui/core';
+import {QR_GENERATOR_ENGINE} from '../../../mm-lib/src/lib/bootstrap/configs/bootstrap.config';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {PagesModule} from './pages/pages.module';
@@ -15,9 +17,14 @@ import {PagesModule} from './pages/pages.module';
         BrowserModule,
         NoopAnimationsModule,
         AppRoutingModule,
+        BootstrapModule.forRoot({
+            grGeneratorEngine: QR_GENERATOR_ENGINE.Client,
+        }),
         PagesModule,
-        TuiRootModule,
+        TuiAlertModule,
         TuiModeModule,
+        TuiRootModule,
+        TuiThemeNightModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
--- projects/mm-lib/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of mm-lib
- */
-
-export * from './lib/mm-lib.service';
-export * from './lib/mm-lib.component';
-export * from './lib/mm-lib.module';
